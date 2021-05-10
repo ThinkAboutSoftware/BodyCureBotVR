b2c52747fb0ba2b6ca26b1c98e7d208d702b2e18
ErigoLee <57980181+ErigoLee@users.noreply.github.com>
2021-05-10 20:18:02 +0900

    BodyCureBotVR

---
+++ Assets/Scripts/Network/NetworkPlayerSpawnerOther.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+
+public class NetworkPlayerSpawnerOther : MonoBehaviourPunCallbacks
+{
+    private GameObject spawnedPlayerPrefab;
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player Other", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawnedPlayerPrefab);
+    }
+}
